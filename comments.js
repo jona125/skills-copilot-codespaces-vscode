b4c01636b50cc87d5d440bfd1524299b76961ea9
Jonathan Che-Pin Chang <36974316+jona125@users.noreply.github.com>
2024-10-14 06:48:43 +0000

    third commit

--- comments.js
+++ comments.js
@@ -3,7 +3,7 @@
 // 2. Create a route
 // 3. Create a route handler
 // 4. Send a response
-
+// Import the http module
 // Load the express module
 const express = require('express');
 
@@ -12,6 +12,7 @@ const app = express();
 
 // Load the comments module
 const comments = require('./comments');
+const http = require('http');
 
 // Create a route for the comments
 app.get('/comments', (req, res) => {
